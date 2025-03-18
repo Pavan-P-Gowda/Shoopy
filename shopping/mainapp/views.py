186306d5bff8f9306bb12b0867d071aa75089a81
Pavan-P-Gowda <pavanpgowda31@gmail.com>
2025-03-18 16:35:54 +0530

    mainapp

--- shopping/mainapp/views.py
+++ shopping/mainapp/views.py
@@ -3,3 +3,8 @@ from django.template import loader
 from django.http import HttpResponse
 # Create your views here.
 def home(request):
+    template = loader.get_template('home.html')
+    context = {
+
+    }
+    return HttpResponse(template.render(context, request))
