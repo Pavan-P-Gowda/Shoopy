577d294af30af344ad30d4a1164a137a534dfb01
Pavan-P-Gowda <pavanpgowda31@gmail.com>
2025-03-21 08:58:27 +0530

    models

--- shopping/mainapp/models.py
+++ shopping/mainapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=200) #varchar in db
+    price = models.FloatField()
+    desc = models.TextField(max_length=500)
+    img = models.ImageField(upload_to='products/')
+    stock = models.PositiveIntegerField()
+    
