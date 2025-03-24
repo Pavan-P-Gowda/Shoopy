130f9e957b7aaf7b77a11257795d4046df5150c6
Pavan-P-Gowda <pavanpgowda31@gmail.com>
2025-03-24 09:01:01 +0530

    models,view

--- shopping/mainapp/models.py
+++ shopping/mainapp/models.py
@@ -9,3 +9,5 @@ class Product(models.Model):
     img = models.ImageField(upload_to='products/')
     stock = models.PositiveIntegerField()
     
+    def __str__(self):
+         return f"Product : {self.name}"
--- shopping/mainapp/views.py
+++ shopping/mainapp/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
+
 from django.template import loader
+
 from django.http import HttpResponse
+
+from .models import Product
 # Create your views here.
 def homeView(request):
     template = loader.get_template('home.html')
     context = {
+        # context data to be pulled from the db
+        'product' : Product.objects.all()
+        #the above line of the code is equivalent to Select * from product_table; 
 
     }
     return HttpResponse(template.render(context, request))
