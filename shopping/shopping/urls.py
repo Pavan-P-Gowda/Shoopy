45b0d6430f9f35de7a296384024a5572a011fee3
Pavan-P-Gowda <pavanpgowda31@gmail.com>
2025-03-25 09:05:47 +0530

    home.html-edited

--- shopping/shopping/urls.py
+++ shopping/shopping/urls.py
@@ -17,7 +17,16 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+# to include media files 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('mainapp.urls'))
 ]
+
+# the folowing line allows us to use given media path diuring develomplment
+if settings.DEBUG == True:
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+# THE above code genertaes unique urls for each media file in the provided media_root loaction 
