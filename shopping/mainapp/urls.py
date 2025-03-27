d186f78155dc66b525716268959f940c37c44632
Pavan-P-Gowda <pavanpgowda31@gmail.com>
2025-03-27 08:15:38 +0530

    added to url and in home

--- shopping/mainapp/urls.py
+++ shopping/mainapp/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # path('<urlpattern>',vie.<viefunction>,name='<path_refercens_name>'), this is for function 
+    #path('<urlpattern>,view.<classbasedname>.as_view(),name=<'pathname>) this is for class
     path('', views.homeView, name = 'homepage'),
     path('about',views.aboutView, name='aboutpage'),
     path('contact',views.contactView, name='contactpage'),
+    path('product/<int:pk>',views.ProductDetails.as_view(),name = 'product_details'),
 ]
