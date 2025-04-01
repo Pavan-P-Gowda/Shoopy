cb485a8b377168afddf255f1322aecbf79182b1a
Pavan-P-Gowda <pavanpgowda31@gmail.com>
2025-04-01 08:59:10 +0530

    added update , delete page and also created update,delete html

--- shopping/mainapp/urls.py
+++ shopping/mainapp/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
-from . import views
-
+from . import views # ". " will say import from this folder file 
 urlpatterns = [
     # path('<urlpattern>',vie.<viefunction>,name='<path_refercens_name>'), this is for function 
     #path('<urlpattern>,view.<classbasedname>.as_view(),name=<'pathname>) this is for class
@@ -8,5 +7,7 @@ urlpatterns = [
     path('about',views.aboutView, name='aboutpage'),
     path('contact',views.contactView, name='contactpage'),
     path('product/<int:pk>',views.ProductDetails.as_view(),name = 'product_details'),
-    path('products/add',views.AddProduct.as_view(), name = 'add_product')
+    path('products/add',views.AddProduct.as_view(), name = 'add_product'),
+    path('product/edit/<int:pk>',views.EditProduct.as_view(), name = 'edit_product'),
+    path('product/delete/<int:pk>' ,views.DeletProduct.as_view(), name = 'delete_product')
 ]
--- shopping/mainapp/views.py
+++ shopping/mainapp/views.py
@@ -37,8 +37,27 @@ def contactView(request):
 #read operations we already exploxed above
 #lets explre the other
 
+# c-create
+
 class AddProduct(CreateView):
     model = Product
     template_name = 'add_product.html'
-    fields = '__all__'
-    success_url ='/'
+    fields = '__all__' # this include all the field in the form
+    # this view on successful addition of product , redirects the user to a
+    #new page , to set this page 
+    success_url ='/' # after succefull it will go it will itself back
+
+# u - upadte
+
+class EditProduct(UpdateView):
+    model = Product 
+    context_object_name = 'product'
+    template_name = "edit_product.html"
+    fields = ['img' , 'price' , 'stock'] # it will show the particular coloum
+    success_url = '/'
+
+# D - delet 
+class DeletProduct(DeleteView):
+    model = Product
+    template_name = 'delet_product.html'
+    success_url = '/'
