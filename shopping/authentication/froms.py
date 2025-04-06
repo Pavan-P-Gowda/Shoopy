4443d1fac973e21051088f2f6a892b754523fc96
Pavan-P-Gowda <pavanpgowda31@gmail.com>
2025-04-06 13:54:22 +0530

    created form,import into view,creted signuphtml at 04,25

--- shopping/authentication/froms.py
+++ shopping/authentication/froms.py
@@ -1,10 +1,13 @@
 #  this files is to building custom forms.
 
+# importing the User model from django's inbuilt authentication app
+from django.contrib.auth.models import User 
+
 #  importing the inbuilt forms 
 from django import forms
 
 # importing authenticcationforms django's inbulit authentication 
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 
 class CustomLoginForm(AuthenticationForm):
     username = forms.CharField(
@@ -13,4 +16,37 @@ class CustomLoginForm(AuthenticationForm):
             'placeholder': 'Enter your username'
         })
     )
-    
+    password = forms.CharField(
+        label = 'Password',
+        widget= forms.PasswordInput(attrs={
+            'class' : 'form-control',
+            'placeholder' : 'Enter your password'
+        })
+    )
+class CustomRegisterForm(UserCreationForm):
+    username = forms.CharField(
+        widget= forms.TextInput(attrs={
+            'class' : 'form-control',
+            'placeholder' : 'Enter your username'
+        })
+    )
+
+    password1 = forms.CharField(
+        label = 'Password',
+        widget= forms.PasswordInput(attrs={
+            'class' : 'form-control',
+            'placeholder' : 'Enter your password'
+        })
+    )
+
+    password2 = forms.CharField(
+        label = 'Confirm Password',
+        widget= forms.PasswordInput(attrs={
+            'class' : 'form-control',
+            'placeholder' : 'Confirm Password'
+        })
+    )
+
+    class Meta:
+        model = User 
+        fields = ('username','password1','password2')
--- shopping/authentication/views.py
+++ shopping/authentication/views.py
@@ -5,17 +5,19 @@ from django.urls import reverse_lazy
 
 
 # importing the inbuilt user regesteration from calss :> usercretaionform
-from django.contrib.auth.forms import UserCreationForm
+from .froms  import CustomLoginForm, CustomRegisterForm
+
 
 # importing the inbuilt user loginview to inherritens and override the from and template
 from django.contrib.auth.views import LoginView
 
 # Create your views here.
 class UserRegister(CreateView):
-    from_class = UserCreationForm 
+    from_class = CustomRegisterForm
     template_name = 'signup.html'
     # find the sig-in page path on succeessfull registaion and sendd the useer there
     success_url = reverse_lazy('singin')
 
 class UserLogin(LoginView):
     template_name = 'signin.html'
+    form_class = CustomLoginForm
