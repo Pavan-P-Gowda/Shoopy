663c47e3f1dd8c89c91aeadd5c77030a442caa49
Pavan-P-Gowda <pavanpgowda31@gmail.com>
2025-04-11 08:24:40 +0530

    created another view and url then linked to home and productdetalis html

--- shopping/cart/views.py
+++ shopping/cart/views.py
@@ -1,10 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.template import loader 
 from .models import CartItem
+from mainapp.models import Product
 # Create your views here.
 
 def viewCart(request):
-    CartItems = CartItem.objects.filter(user = request.user)
+    CartItems = CartItem.objects.filter(user = request.user) 
+    # filter means select * from cartitem where 
 
     template = 'cart.html'
 
@@ -12,3 +14,16 @@ def viewCart(request):
         'items' : CartItems
     }
     return render(request,template,context)
+
+def addToCart(request, product_id):
+    this_product = Product.objects.get(id = product_id)
+    # get means select * from product whrer id= product_id,
+    # it will referecen to indidvidual object (get)
+
+    cart_item, created_id = CartItem.objects.get_or_create(product =  this_product, user = request.user)
+    # getor create will help when cart is not added it will add r when cart is already there it will update automaticaly
+
+    cart_item.quantity +=1
+    cart_item.save()
+
+    return redirect('view_cart')
