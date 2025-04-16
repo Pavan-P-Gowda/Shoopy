ebfc5b624bd8082ca1d36b8ed9de89b8f6065d1f
Pavan-P-Gowda <pavanpgowda31@gmail.com>
2025-04-16 15:08:01 +0530

    creted remove button added to url,in carthtml link added  and added shop more in cart html then creted total price in the viewcart

--- shopping/cart/urls.py
+++ shopping/cart/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path 
-from .views import viewCart,addToCart
+from .views import viewCart, addToCart, remFromCart
 
 urlpatterns = [
     path('', viewCart, name = 'view_cart'),
-    path('addcart/<int:product_id>', addToCart, name= 'add_to_cart')
+    path('addcart/<int:product_id>', addToCart, name= 'add_to_cart'),
+    path('remcart/<int:cart_item_id>', remFromCart, name = 'rem_from_cart'), # that after 'int' we write for function based to get particular id not for class baseed
 ]
--- shopping/cart/views.py
+++ shopping/cart/views.py
@@ -2,19 +2,30 @@ from django.shortcuts import render, redirect
 from django.template import loader 
 from .models import CartItem
 from mainapp.models import Product
+
+from django.contrib.auth.decorators import login_required 
+# in above 'decortators' will help not to show something error for user when then reload r 
+# without login if they add r buy the things soo we import 'login_requried' and add it above every function
+
 # Create your views here.
 
+@login_required
 def viewCart(request):
     CartItems = CartItem.objects.filter(user = request.user) 
-    # filter means select * from cartitem where 
+    # filter means select * from cartitem where  user = request.user ,
+    # this gives reference to collection of the items (filter)
 
+    total_price = sum([float(item.product.price)* item.quantity for item in CartItems]) 
+    # this is used for  sum the total items in the my cart 
     template = 'cart.html'
 
     context = {
-        'items' : CartItems
+        'items' : CartItems,
+        'total' : total_price
     }
     return render(request,template,context)
 
+@login_required
 def addToCart(request, product_id):
     this_product = Product.objects.get(id = product_id)
     # get means select * from product whrer id= product_id,
@@ -27,3 +38,10 @@ def addToCart(request, product_id):
     cart_item.save()
 
     return redirect('view_cart')
+
+@login_required
+def remFromCart(request, cart_item_id) :
+    this_cart_item = CartItem.objects.get(id = cart_item_id)
+    this_cart_item.delete() #this line will help to delete the cart_item_object and recode in the cartitem table in dbs
+
+    return redirect('view_cart') # return will help go back to viewcart after succeeful deletion of item
