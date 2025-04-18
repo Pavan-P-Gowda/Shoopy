3f5973f8b842e873472a8a55fe4d902cf2cc9d1e
Pavan-P-Gowda <pavanpgowda31@gmail.com>
2025-04-18 08:45:06 +0530

    creted add plus and remove in cart view.py

--- shopping/cart/views.py
+++ shopping/cart/views.py
@@ -7,6 +7,9 @@ from django.contrib.auth.decorators import login_required
 # in above 'decortators' will help not to show something error for user when then reload r 
 # without login if they add r buy the things soo we import 'login_requried' and add it above every function
 
+from django.http import JsonResponse
+from django.shortcuts import get_object_or_404
+
 # Create your views here.
 
 @login_required
@@ -32,7 +35,7 @@ def addToCart(request, product_id):
     # it will referecen to indidvidual object (get)
 
     cart_item, created_id = CartItem.objects.get_or_create(product =  this_product, user = request.user)
-    # getor create will help when cart is not added it will add r when cart is already there it will update automaticaly
+    # get_or_create will help when cart is not added it will add r when cart is already there it will update automaticaly
 
     cart_item.quantity +=1
     cart_item.save()
@@ -45,3 +48,40 @@ def remFromCart(request, cart_item_id) :
     this_cart_item.delete() #this line will help to delete the cart_item_object and recode in the cartitem table in dbs
 
     return redirect('view_cart') # return will help go back to viewcart after succeeful deletion of item
+
+@login_required
+def  addQuantity(request, cart_item_id):
+    cart_item = get_object_or_404(CartItem, id=cart_item_id, user= request.user)
+    cart_item.quantity += 1
+    cart_item.save()
+    overall_total= sum(item.get_tottal() for item in CartItem.objects.filter(user=request.user))
+    context = {
+        'quantity' : cart_item.quantity,
+        'total_price' : cart_item.get_total(),
+        'over_all_total' : overall_total
+    }
+    return JsonResponse(context)
+
+@login_required
+def remQuantity(request, cart_item_id):
+    cart_item = get_object_or_404(CartItem,id = cart_item_id,user= request.user)
+    if cart_item.quantity > 1:
+        cart_item.quantity -=1
+        cart_item.save()
+        overall_total= sum(item.get_tottal() for item in CartItem.objects.filter(user=request.user))
+        context = {
+        'quantity' : cart_item.quantity,
+        'total_price' : cart_item.get_total(),
+        'over_all_total' : overall_total
+        }
+        return JsonResponse(context)
+    else:
+        cart_item.delete()
+        overall_total= sum(item.get_tottal() for item in CartItem.objects.filter(user=request.user))
+        context = {
+        'quantity' : 0,
+        'total_price' : 0,
+        'over_all_total' : overall_total
+        }
+        return JsonResponse(context)
+        
